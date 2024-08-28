e80c44b85519a4dbe13fb9ceaa47bbdfbbd61d40
Mikhail96-Chuprov <m.chuprov96@yandex.ru>
2024-08-28 18:34:50 +0300

    Задание 1 выполнено

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,12 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var labelCount: UILabel!
+    @IBOutlet weak var ButtonClick: UIButton!
+    
+    var count = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func buttonCount(_ sender: Any) {
+        print("Нажатие")
+        count = count + 1
+        labelCount.text = "Значение счётчика: \(count)"
+    }
+    
 }
 
